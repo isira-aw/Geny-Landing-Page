b1aa34e834fd6c9e5a8ce5a14daff6a67e47f01e
Isira Adithya <isira.weerasinghe@arimaclanka.com>
2025-08-14 20:43:57 +0530

    background color

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface ContentBoxProps {
   children: React.ReactNode;
@@ -11,10 +11,10 @@ export const ContentBox: React.FC<ContentBoxProps> = ({ children }) => {
     //     {children}
     //   </div>
     // </div>
-    <div className="relative w-full max-w-md md:max-w-lg mt-10 lg:max-w-xl h-[80vh] rounded-2xl p-6 md:p-8 animate-fade-in overflow-y-auto overflow-x-hidden fade-mask">
-  <div className="text-white">
-    {children}
-  </div>
+    
+
+<div className="relative w-full max-w-md md:max-w-lg mt-10 lg:max-w-xl h-[80vh] rounded-2xl p-6 md:p-8 animate-fade-in overflow-y-auto overflow-x-hidden fade-mask max-[450px]:bg-[rgba(15,15,15,0.8)] max-[450px]:max-w-full max-[450px]:h-screen max-[450px]:rounded-none max-[450px]:mt-0 max-[450px]:p-4">
+  <div className="text-white">{children}</div>
 </div>
   );
 };
